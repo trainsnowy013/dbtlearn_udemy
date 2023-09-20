91979e511bac10f227035fcea8289c94e5b9c58a
Adrian Wilfred Elsisura <aelsisura@yondu.com>
2023-09-20 12:09:12 +0800

    ''

---
+++ models/src/src_listings.sql
@@ -0,0 +1,23 @@
+{{
+config(
+materialized = 'view'
+)
+}}
+WITH raw_listings AS (
+SELECT
+*
+FROM
+{{source('airbnb','listings')}}
+)
+SELECT
+id AS listing_id,
+name AS listing_name,
+listing_url,
+room_type,
+minimum_nights,
+host_id,
+price AS price_str,
+created_at,
+updated_at
+FROM
+raw_listings
